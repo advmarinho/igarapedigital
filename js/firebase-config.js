de42902357a2c3f0f67800e009740c0d422fc6b5
Anderson Marinho <marinho.1983@gmail.com>
2025-08-03 23:27:16 -0300

    Ajustado Firebase para ambiente de produção real

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,29 @@
+// // js/firebase-config.js
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+// import { getDatabase, connectDatabaseEmulator } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAYuPvTzXWsr85ggPwyjQSlH-P-CY328Jw",
+//   authDomain: "sorteador-igarape-digital.firebaseapp.com",
+//   databaseURL: "https://sorteador-igarape-digital-default-rtdb.firebaseio.com",
+//   projectId: "sorteador-igarape-digital",
+//   storageBucket: "sorteador-igarape-digital.appspot.com",
+//   messagingSenderId: "212600875536",
+//   appId: "1:212600875536:web:c8c6dc9345c724e5917895"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const db  = getDatabase(app);
+
+// // Sempre chame antes de qualquer push()/onValue()
+// connectDatabaseEmulator(db, location.hostname, 9000);
+// console.log("🟢 Conectado ao Emulator em:", location.hostname, "porta 9000");
+
+// export { db };
+
 // js/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getDatabase, connectDatabaseEmulator } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAYuPvTzXWsr85ggPwyjQSlH-P-CY328Jw",
@@ -15,9 +38,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db  = getDatabase(app);
 
-// Sempre chame antes de qualquer push()/onValue()
-connectDatabaseEmulator(db, location.hostname, 9000);
-console.log("🟢 Conectado ao Emulator em:", location.hostname, "porta 9000");
+console.log("🌐 Firebase conectado em ambiente de produção");
 
 export { db };
+
 
